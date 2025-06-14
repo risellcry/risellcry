4ba926aff108b6fbd8f70b77fb415312e803963b
Rise LLC <rise.llc.ry@gmail.com>
2025-06-14 08:29:58 +0000

    Fixed some bugs

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+const info = document.getElementById("info");
 const cart_2de = document.getElementById("cart-2de");
 
 cart_2de.onclick(function (event) {
-    alert("Sorry, 2DE is currently working right now and cannot be bought until the project finishes.");
+    info.style.visibility = "visible";
 });
